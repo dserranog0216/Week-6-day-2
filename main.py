6f1aa0d36dec40e18f985e9039fc9ba8a8c63860
David Serrano <113551943+dserranog0216@users.noreply.github.com>
2022-09-29 20:18:48 +0000

    For loops practice 1

--- main.py
+++ main.py
@@ -291,7 +291,8 @@ for name in my_list:
 
 students = ["Norville", "Fred", "Velma", "Daphne"]
 
-
+for student in students:
+  print(f"Hello {student}")
 
 
 # For Loops Practice #2
@@ -300,7 +301,8 @@ students = ["Norville", "Fred", "Velma", "Daphne"]
 list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
 # sum_numbers = 
 
-
+for sum in list_numbers:
+  
 
 
 
